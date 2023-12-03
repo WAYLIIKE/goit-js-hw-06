1726601082fec9adf6f874efba974bc463b53db1
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-03 02:45:48 +0100

    Task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let ingredientsList = ingredients.map((ingredient) => {
+  const list = document.createElement("li");
+  list.textContent = ingredient;
+  list.classList.add("item");
+  return list;
+});
+
+const list = document.querySelector("#ingredients");
+
+list.append(...ingredientsList);
