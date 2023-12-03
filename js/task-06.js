d045290305c1273f2fa58cbdfa081c308f6d1676
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-04 00:32:11 +0100

    Completed task 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", borderAttention);
+
+function borderAttention(event) {
+  const validateNumb = Number(input.getAttribute("data-length"));
+  if (event.target.value.length > validateNumb) {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  } else if (event.target.value.length === validateNumb) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else input.classList.remove("invalid", "valid");
+}
