e9573630904b91962d05d9924eaea9b36dfb40e4
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-04 02:34:02 +0100

    Completed optional task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const mainDiv = document.querySelector("#boxes");
+
+const createBtn = document.querySelector("button[data-create]");
+
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+const input = document.querySelector("input");
+
+createBtn.addEventListener("click", function () {
+  const amount = parseInt(input.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  mainDiv.innerHTML = "";
+  for (let i = 0; i < amount * 10; i += 10) {
+    let color = getRandomHexColor();
+    let div = document.createElement("div");
+    div.style.width = 30 + i + "px";
+    div.style.height = 30 + i + "px";
+    div.style.backgroundColor = color;
+    mainDiv.append(div);
+  }
+}
+
+function destroyBoxes() {
+  mainDiv.innerHTML = "";
+}
