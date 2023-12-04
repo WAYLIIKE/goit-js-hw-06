1ed3fadc28ab5d553eba285f23c8e6ab01d9989e
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-04 01:36:20 +0100

    Completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+
+const btn = document.querySelector(".change-color");
+
+const colorHex = document.querySelector(".color");
+
+btn.addEventListener("click", handlerbtn);
+
+function handlerbtn() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorHex.textContent = color;
+}
