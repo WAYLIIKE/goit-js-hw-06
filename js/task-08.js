d53b230330f198c4c1ede4bc06a52bba74bf1879
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-04 01:14:25 +0100

    Completed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", formHundle);
+
+function formHundle(event) {
+  event.preventDefault();
+  const { email, password } = event.target.elements;
+  let obj = {};
+  if (email.value === "" || password.value === "") {
+    alert("Enter email and password!");
+  } else console.log((obj = { email: email.value, password: password.value }));
+  event.target.reset();
+}
